2dfd4bc375e9e1b1cc1cafe7ebfcca7da1fc2978
mozoroff <mozoshima@gmail.com>
2023-01-12 14:12:30 +0900

    Initial Commit

---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by kensho on 2023/01/12.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
