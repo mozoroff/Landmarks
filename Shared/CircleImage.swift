db55a3892f939877a1cfcb9f4344c0a497f77996
mozoroff <mozoshima@gmail.com>
2023-01-12 15:18:28 +0900

    他のフレームワークからの SwiftUI ビューの使用

--- Shared/CircleImage.swift
+++ Shared/CircleImage.swift
@@ -7,13 +7,15 @@
 
 import SwiftUI
 
+//カスタム イメージ ビューを作成する、円形にマスクしドロップシャドウと線をつけた
 struct CircleImage: View {
     var body: some View {
-        Image("turtlerock")
-            .clipShape(Circle())
-            .overlay{
+        Image("turtlerock") //アセットに登録した画像の呼び出し
+            .clipShape(Circle()) //円型にマスク
+            .overlay{ //円型の線をつける
                 Circle().stroke(.white, lineWidth: 4)
             }
+            //イメージ全体に影をつける
             .shadow(radius:7)
     }
 }
