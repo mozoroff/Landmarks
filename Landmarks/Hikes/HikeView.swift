5654a4d7f8b62024ca594a770b067e3cc21ba1b6
mozoroff <mozoshima@gmail.com>
2023-01-16 15:09:16 +0900

    ビューの遷移をカスタマイズする

--- Landmarks/Hikes/HikeView.swift
+++ Landmarks/Hikes/HikeView.swift
@@ -1,30 +1,41 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-A view displaying information about a hike, including an elevation graph.
-*/
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ A view displaying information about a hike, including an elevation graph.
+ */
 
 import SwiftUI
+//AnyTransitionの静的プロパティとして追加した遷移を抽出し、ビューの遷移修飾子で新しいプロパティにアクセスします。
+// extensionとは、クラス・構造体・列挙体に定義を追加する
+extension AnyTransition {
+    static var moveAndFade: AnyTransition {
+        //asymmetric(insertion:removal:)修飾子を使用して、ビューが表示されたり消えたりするときに異なる遷移を提供します
+        .asymmetric(
+            insertion: .move(edge: .trailing).combined(with: .opacity),
+            removal: .scale.combined(with: .opacity)
+        )
+    }
+}
 
 struct HikeView: View {
     var hike: Hike
     @State private var showDetail = false
-
+    
     var body: some View {
         VStack {
             HStack {
                 HikeGraph(hike: hike, path: \.elevation)
                     .frame(width: 50, height: 30)
-
+                
                 VStack(alignment: .leading) {
                     Text(hike.name)
                         .font(.headline)
                     Text(hike.distanceText)
                 }
-
+                
                 Spacer()
-
+                
                 Button {
                     //withAnimation 関数の呼び出しでshowDetail.toggle()の呼び出しをラップします。
                     //showDetail プロパティの影響を受ける両方のビュー (開示ボタンと HikeDetail ビュー) は、アニメーション化されたトランジションを持つようになりました。
@@ -41,9 +52,10 @@ struct HikeView: View {
                         .padding()
                 }
             }
-
+            
             if showDetail {
                 HikeDetail(hike: hike)
+                    .transition(.moveAndFade)
             }
         }
     }
